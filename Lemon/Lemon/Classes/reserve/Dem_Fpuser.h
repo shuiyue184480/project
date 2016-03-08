8ea56ab320924d3a68fae3f161012fa87836e0fa
a761552360 <761552360@qqc.com>
2016-03-08 13:54:47 +0800

    评论接口

---
+++ Lemon/Lemon/Classes/reserve/Dem_Fpuser.h
@@ -0,0 +1,16 @@
+//
+//  Dem_Fpuser.h
+//  Lemon
+//
+//  Created by lanou3g on 16/3/7.
+//  Copyright © 2016年 Demon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+@interface Dem_Fpuser : NSObject
+
+@property(nonatomic,copy)NSString *content;
+@property(nonatomic,strong)UIImage *img;
+
+@end
